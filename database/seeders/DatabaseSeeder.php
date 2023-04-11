11fceaa39baf755c2a5c0552fe67df1bcf3193ab
AIIA\fabian <fabian@aiia.co.id>
2023-04-11 12:28:35 +0630

    dev-1.0.0: fix minor bug

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,86 +23,86 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        \App\Models\TmArea::create([
+        TmArea::create([
             'name' => 'Warehouse'
         ]);
-        \App\Models\TmArea::create([
+        TmArea::create([
             'name' => 'DC'
         ]);
-        \App\Models\TmArea::create([
+        TmArea::create([
             'name' => 'MA'
         ]);
-        \App\Models\TmArea::create([
+        TmArea::create([
             'name' => 'ASSY'
         ]);
 
-        // TmTransaction::create([
-        //     'code' => '111',
-        //     'name' => 'STO',
-        //     'type' => 'supply',
-        // ]);
+        TmTransaction::create([
+            'code' => '111',
+            'name' => 'STO',
+            'type' => 'supply',
+        ]);
 
-        // TmTransaction::create([
-        //     'code' => '112',
-        //     'name' => 'STO (R)',
-        //     'type' => 'checkout',
-        // ]);
+        TmTransaction::create([
+            'code' => '112',
+            'name' => 'STO (R)',
+            'type' => 'checkout',
+        ]);
 
-        // TmTransaction::create([
-        //     'code' => '211',
-        //     'name' => 'Unboxing',
-        //     'type' => 'supply',
-        // ]);
+        TmTransaction::create([
+            'code' => '211',
+            'name' => 'Unboxing',
+            'type' => 'supply',
+        ]);
 
-        // TmTransaction::create([
-        //     'code' => '212',
-        //     'name' => 'Unboxing (R)',
-        //     'type' => 'checkout',
-        // ]);
-        // TmTransaction::create([
-        //     'code' => '311',
-        //     'name' => 'Pulling Production',
-        //     'type' => 'supply',
-        // ]);
+        TmTransaction::create([
+            'code' => '212',
+            'name' => 'Unboxing (R)',
+            'type' => 'checkout',
+        ]);
+        TmTransaction::create([
+            'code' => '311',
+            'name' => 'Pulling Production',
+            'type' => 'supply',
+        ]);
 
-        // TmTransaction::create([
-        //     'code' => '312',
-        //     'name' => 'Pulling Production (R)',
-        //     'type' => 'checkout',
-        // ]);
-        // TmTransaction::create([
-        //     'code' => '411',
-        //     'name' => 'Pulling Delivery',
-        //     'type' => 'supply',
-        // ]);
+        TmTransaction::create([
+            'code' => '312',
+            'name' => 'Pulling Production (R)',
+            'type' => 'checkout',
+        ]);
+        TmTransaction::create([
+            'code' => '411',
+            'name' => 'Pulling Delivery',
+            'type' => 'supply',
+        ]);
 
-        // TmTransaction::create([
-        //     'code' => '412',
-        //     'name' => 'Pulling Delivery (R)',
-        //     'type' => 'checkout',
-        // ]);
-        // TmTransaction::create([
-        //     'code' => '511',
-        //     'name' => 'Traceability',
-        //     'type' => 'supply',
-        // ]);
+        TmTransaction::create([
+            'code' => '412',
+            'name' => 'Pulling Delivery (R)',
+            'type' => 'checkout',
+        ]);
+        TmTransaction::create([
+            'code' => '511',
+            'name' => 'Traceability',
+            'type' => 'supply',
+        ]);
 
-        // TmTransaction::create([
-        //     'code' => '512',
-        //     'name' => 'Traceability (R)',
-        //     'type' => 'checkout',
-        // ]);
-        // TmTransaction::create([
-        //     'code' => '611',
-        //     'name' => 'NG Judgement',
-        //     'type' => 'supply',
-        // ]);
+        TmTransaction::create([
+            'code' => '512',
+            'name' => 'Traceability (R)',
+            'type' => 'checkout',
+        ]);
+        TmTransaction::create([
+            'code' => '611',
+            'name' => 'NG Judgement',
+            'type' => 'supply',
+        ]);
 
-        // TmTransaction::create([
-        //     'code' => '612',
-        //     'name' => 'NG Judgement (R)',
-        //     'type' => 'checkout',
-        // ]);
+        TmTransaction::create([
+            'code' => '612',
+            'name' => 'NG Judgement (R)',
+            'type' => 'checkout',
+        ]);
 
     }
 }
