1921dd7307ffbbd1d651e3b7293115b0d1e84f92
AIIA\fabian <fabian@aiia.co.id>
2023-04-14 06:48:33 +0630

    dev-1.0.0: coba  API

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -187,7 +187,7 @@ class StockController extends Controller
             $opn = getWipDc('OPN');
 
             // wip data
-            $wipData = [$tcc->current_stock,$opn->current_stock];
+            $wipData = [$tcc,$opn];
 
             // push to websocket
             $this->pushData('wip', $wipData);
