c4062436955c0c2695abe6a8877d7f124b479bf9
AIIA\fabian <fabian@aiia.co.id>
2023-07-22 13:10:47 +0700

    dev-1.0.0: update minor

--- app/Http/Controllers/FgController.php
+++ app/Http/Controllers/FgController.php
@@ -77,11 +77,11 @@ class FgController extends Controller
     public function fgAssyGetTransaction()
     {
         // get from ttassy
-        $input = DB::table('tt_mas')
-                ->join('tm_parts', 'tm_parts.id', '=', 'tt_mas.id_part')
-                ->join('tm_transactions', 'tm_transactions.id', '=', 'tt_mas.id_transaction')
-                ->select('tm_parts.part_name', 'tm_parts.part_number', 'tm_transactions.name','tm_transactions.type' , 'tt_mas.pic', 'tt_mas.date', 'tt_mas.qty')
-                ->where('tm_parts.status', 1)
+        $input = DB::table('tt_assy')
+                ->join('tm_parts', 'tm_parts.id', '=', 'tt_assy.id_part')
+                ->join('tm_transactions', 'tm_transactions.id', '=', 'tt_assy.id_transaction')
+                ->select('tm_parts.part_name', 'tm_parts.part_number', 'tm_transactions.name','tm_transactions.type' , 'tt_assy.pic', 'tt_assy.date', 'tt_assy.qty')
+                ->where('tm_parts.status', 2)
                 ->get();
 
         return DataTables::of($input)
